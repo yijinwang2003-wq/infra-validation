c03a5425bd7adafaf711715dc55a36f5e267384e
Yijin Wang <yijin.wang2003@gmail.com>
2026-02-27 22:43:46 -0600

    Initial secure version

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "Security group for infra validation demo"
+
+  ingress {
+    description = "Allow HTTP from internal network only"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Restrict outbound traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
